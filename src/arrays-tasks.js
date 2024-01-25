576f0b0da8d8b386b2bcc0e140b4314e6f113c15
Alexander-1092 <Alex-Sadr1092@yandex.ru>
2024-01-25 12:33:39 +0300

    init: start ptoj

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -20,8 +20,13 @@
  *    getIntervalArray(0, 100) => [ 0, 1, 2, ..., 100 ]
  *    getIntervalArray(3, 3) => [ 3 ]
  */
-function getIntervalArray(/* start, end */) {
-  throw new Error('Not implemented');
+function getIntervalArray(start, end) {
+  const rez = Array.from({ length: end - start + 1 });
+  let counter = start - 1;
+  return rez.map(() => {
+    counter += 1;
+    return counter;
+  });
 }
 
 /**
@@ -53,8 +58,8 @@ function sumArrays(/* arr1, arr2 */) {
  *    findElement(['Array', 'Number', 'string'], 'Date') => -1
  *    findElement([0, 1, 2, 3, 4, 5], 5) => 5
  */
-function findElement(/* arr, value */) {
-  throw new Error('Not implemented');
+function findElement(arr, value) {
+  return arr.indexOf(value) ? arr.indexOf(value) : -1;
 }
 
 /**
@@ -71,8 +76,13 @@ function findElement(/* arr, value */) {
  *    findAllOccurrences([ null, undefined, null ], null) => 2
  *    findAllOccurrences([ true, 0, 1, 'true' ], true) => 1
  */
-function findAllOccurrences(/* arr, item */) {
-  throw new Error('Not implemented');
+function findAllOccurrences(arr, item) {
+  return arr.filter((elem) => {
+    if (elem === item) {
+      return String(item);
+    }
+    return null;
+  }).length;
 }
 
 /**
@@ -87,8 +97,16 @@ function findAllOccurrences(/* arr, item */) {
  *    removeFalsyValues([ 1, 2, 3, 4, 5, 'false' ]) => [ 1, 2, 3, 4, 5, 'false' ]
  *    removeFalsyValues([ false, 0, NaN, '', undefined ]) => [ ]
  */
-function removeFalsyValues(/* arr */) {
-  throw new Error('Not implemented');
+function removeFalsyValues(arr) {
+  const newArr = arr.filter(
+    (elem) =>
+      elem !== 0 &&
+      elem !== false &&
+      elem !== '' &&
+      !Number.isNaN(elem) &&
+      elem !== undefined
+  );
+  return newArr;
 }
 
 /**
